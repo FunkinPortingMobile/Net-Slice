cba8b59770f1ffbba3c2b486a45c06460e632881
StarNovaBR <cherrylegal001@gmail.com>
2026-09-10 10:40:09 -0300

    hi

--- source/funkin/api/discord/DiscordClient.hx
+++ source/funkin/api/discord/DiscordClient.hx
@@ -2,7 +2,6 @@ package funkin.api.discord;
 
 #if FEATURE_DISCORD_RPC
 import hxdiscord_rpc.Discord;
-import hxdiscord_rpc.Types.DiscordButton;
 import hxdiscord_rpc.Types.DiscordEventHandlers;
 import hxdiscord_rpc.Types.DiscordRichPresence;
 import hxdiscord_rpc.Types.ActivityType;
